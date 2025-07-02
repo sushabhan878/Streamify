6839c5aa32b243eb1772f603b074966b95619ad1
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-02 23:16:16 +0530

    Added Styling for right side layout for all the routes

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const PersonalRoom = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>Personal Room</h1>
+    </section>
   )
 }
 
--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Previous = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>Previous</h1>
+    </section>
   )
 }
 
