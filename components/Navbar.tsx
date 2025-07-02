0a692d8449bef7f8a1eccbe4936138f73e62de09
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-03 00:08:19 +0530

    Implimented the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/icons/logo.svg" alt="Logo" height={32} width={32} className="max-sm:size-10" />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Yoom</p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* Clerk user management TODO */}
+        MobileNav
+      </div>
+    </nav>
   )
 }
 
