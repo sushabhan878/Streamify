6797ffcca37f18935abbee66c877f8ca2336eec2
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-03 21:40:30 +0530

    Added the stream package and .env

---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,22 @@
+import {
+    StreamCall,
+    StreamVideo,
+    StreamVideoClient,
+    User,
+} from "@stream-io/video-react-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call("default", "my-first-call");
+call.join({ create: true });
+
+export const MyApp = () => {
+    return (
+        <StreamVideo client={client}>
+            <StreamCall call={call}>
+        /* <MyVideoUI /> */
+            </StreamCall>
+        </StreamVideo>
+    );
+};
