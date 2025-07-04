94edb9ef5fa9ecf2ebbc38db8212f42a86c9f2a3
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-04 12:27:49 +0530

    Video preview done

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,8 +1,25 @@
+"use client"
+import { VideoPreview, useCall } from "@stream-io/video-react-sdk"
+import { useEffect, useState } from "react"
 
 const MeetingSetup = () => {
+    const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false)
+    const call = useCall()
+    if (!call) throw new Error("Usecall must be used within StreamCall component")
+    useEffect(() => {
+        if (isMicCamToggleOn) {
+            call?.camera.disable()
+            call?.microphone.disable()
+        } else {
+            call?.camera.enable()
+            call?.microphone.enable()
+        }
+    }, [isMicCamToggleOn, call?.camera, call?.microphone])
+
     return (
-        <div>
-            Meeting Setup
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+            <h1 className="text-2xl font-bold">Setup</h1>
+            <VideoPreview />
         </div>
     )
 }
