a953d5bdc3e2c4ec43c9d3140798388ce57cf449
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-04 11:55:25 +0530

    added useGetCallById hook and implimented tit

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,16 +1,22 @@
 "use client"
 import MeetingRoom from '@/components/MeetingRoom'
 import MeetingSetup from '@/components/MeetingSetup'
+import { useGetCallById } from '@/hooks/useGetCallById'
 import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
+import Loader from '@/components/Loader'
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser()
   const [isSetupComplete, setIsSetupComplete] = useState(false)
+  const { call, isCallLoading } = useGetCallById(id)
+
+  if (!isCallLoading || !isLoaded) return <Loader />
   return (
     <main className='h-screen w-full'>
-      <StreamCall>
+      <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
             <MeetingSetup />
