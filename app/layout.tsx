2288ad212ab6250d15e3482a1a50b585124729ea
Sushabhan <sushabhanmajumder878@gmail.com>
2025-07-04 14:22:08 +0530

    Implimented the call layout, settings, and call options

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import "@stream-io/video-react-sdk/dist/css/styles.css"
 import { Toaster } from "@/components/ui/sonner"
 const geistSans = Geist({
   variable: "--font-geist-sans",
